5a904ce36eddca47ad5a3436954126b06e1767f1
minhhien05 <hienvmps13917@fpt.edu.vn>
2021-08-26 11:53:15 +0700

    Add empty porject

---
+++ SoKoBanGame/Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float smoothtimeX, smoothtimeY;
+    public Vector2 velocity;
+    public Vector2 minpos, maxpos;
+    public bool bound;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(this.transform.position.x, player.transform.position.x, ref velocity.x, smoothtimeX);
+        float posY = Mathf.SmoothDamp(this.transform.position.y, player.transform.position.y, ref velocity.y, smoothtimeY);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if (bound)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minpos.x, maxpos.x),
+                Mathf.Clamp(transform.position.y, minpos.y, maxpos.y),
+                Mathf.Clamp(transform.position.z, transform.position.z, transform.position.z));
+        }
+    }
+}
