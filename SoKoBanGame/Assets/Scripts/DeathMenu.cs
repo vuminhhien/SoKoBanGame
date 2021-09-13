098010082e88d535d2545a12353dfb0520322605
minhhien <vuminhhien05@gmail.com>
2021-09-13 18:45:46 +0700

    Xử lý nước và lửa

---
+++ SoKoBanGame/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+       if(player != null){
+           pauseUI.SetActive(true);
+           Time.timeScale = 0;
+       }
+   }
+}
---
+++ SoKoBanGame/Assets/Scripts/RestartAndHome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartAndHome : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene("Scenes/MainScene"); // quay về lại nơi bắt đầu của màn chơi đó
+        Time.timeScale = 1;
+    }
+    public void Home()
+    {
+        
+    }
+}
